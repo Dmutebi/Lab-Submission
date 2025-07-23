6ed0c2552d0d502ebf9d11358e8d7fde8cf0ce4f
Dmutebi <dembe743.845@gmail.com>
2025-07-22 22:57:29 -0400

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from google import genai
+
+# Title of App
+st.title("Chinese Language Learning")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team XX, Web Development - Section X")
+st.subheader("Dembe Mutebi")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. Home Page
+2. Characters Data
+3. Choose a Text!
+4. AI Chatbot-->Translating between both languages
+
+""")
+
